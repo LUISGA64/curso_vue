b273e2f7c50c42bbe778158ea2c7c631ba54168a
LUISGA64 <Luisga6129>
2021-10-28 17:26:23 -0500

    functions

--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
 // function saludar(xyz) {
 //     return `hola ${xyz}`
 // }
-const saludar = function(xyz) {
-    return `Hola: ${xyz}`
-}
 
-const nombre = 'Luisga'
 
-console.log(saludar(nombre))
+// const saludar = function(xyz) {
+//     return `Hola: ${xyz}`
+// }
+
+const saludar = (nombre = 'Peter') => `Hola: ${nombre}`
+
+
+//const nombre = 'Luisga'
+
+//console.log(saludar())
+
+const getUser = () => ({
+    uid: 'ABC123',
+    username: 'Tony001'
+})
+
+
+//console.log(getUser())
+
+const heroes = [{
+    id: 1,
+    name: 'Batman'
+}, {
+    id: 2,
+    name: 'Superman'
+}]
